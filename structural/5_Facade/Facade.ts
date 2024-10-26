2468be8347a9b97318874fccf17f44659c8085f4
Viktor Kniahnitskyi <ndrwkn2001@gmail.com>
2024-10-27 02:06:46 +0300

    Flyweight
    MR issues resolving: move types and private methods outside out of Facade class

--- structural/5_Facade/Facade.ts
+++ structural/5_Facade/Facade.ts
@@ -1,57 +1,22 @@
-import axios from 'axios';
-import { parseString } from 'xml2js';
-
-enum Operation {
-  'Add' = 'Add',
-  'Divide' = 'Divide',
-  'Multiply' = 'Multiply',
-  'Subtract' = 'Subtract',
-}
+import { Operation } from './types';
+import { sendRequest } from './utils';
 
 // Facade for dneonline.com
 class Facade {
-  private getXmlForRequest(a: number, b: number, operation: Operation) {
-    return `<?xml version="1.0" encoding="utf-8"?>
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-  <soap12:Body>
-    <${operation} xmlns="http://tempuri.org/">
-      <intA>${a}</intA>
-      <intB>${b}</intB>
-    </${operation}>
-  </soap12:Body>
-</soap12:Envelope>`;
-  }
-
-  private sendRequest(a: number, b: number, operation: Operation): Promise<number> {
-    const xml = this.getXmlForRequest(a, b, operation);
-
-    return axios('http://www.dneonline.com/calculator.asmx', {
-      method: 'POST',
-      data: xml,
-      headers: { 'Content-Type': 'application/soap+xml; charset=utf-8' },
-    }).then(({ data }) => {
-      return new Promise((resolve) => {
-        parseString(data, function (err, result) {
-          resolve(+result['soap:Envelope']['soap:Body'][0][`${operation}Response`][0][`${operation}Result`][0]);
-        });
-      });
-    });
-  }
-
   add(a: number, b: number): Promise<number> {
-    return this.sendRequest(a, b, Operation.Add);
+    return sendRequest(a, b, Operation.Add);
   }
 
   divide(a: number, b: number): Promise<number> {
-    return this.sendRequest(a, b, Operation.Divide);
+    return sendRequest(a, b, Operation.Divide);
   }
 
   multiply(a: number, b: number): Promise<number> {
-    return this.sendRequest(a, b, Operation.Multiply);
+    return sendRequest(a, b, Operation.Multiply);
   }
 
   subtract(a: number, b: number): Promise<number> {
-    return this.sendRequest(a, b, Operation.Subtract);
+    return sendRequest(a, b, Operation.Subtract);
   }
 }
 
